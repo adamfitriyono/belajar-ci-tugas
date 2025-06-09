a4d016ea3e7551c7fe3d1b2be83e75692f5f05a4
adamfitriyono <adamfitriyono@gmail.com>
2025-06-09 12:55:18 +0700

    menambahkan data transaksi pembelian

--- app/Views/v_contact.php
+++ app/Views/v_contact.php
@@ -1,13 +1,15 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content') ?>
-Ini halaman contact
         <!-- nambah sendiri pakai boostrap -->
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="exampleFormControlInput1" class="form-label">Nama email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nama123@contoh.com">
         </div>
             <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Massage</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Trimakasih sudah membantu..."></textarea>
+        </div>
+        <div class="text">
+            <button type="submit" class="btn btn-primary">Kirim</button>
         </div>
 <?= $this->endSection() ?>
